f483fca2cd78f829b6473bbac32064d0725cae17
saverionapolitano <saverionapolitano@SAVERIONAPO60F4>
2025-04-11 23:05:05 +0200

    update

---
+++ exams/10/01/hufstr.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <cstdint>
+class hufstr {
+    struct elem {
+        uint8_t _sym;
+        uint8_t _len = 0;
+        uint32_t _code = 0;
+        elem(uint8_t sym, uint8_t len, uint32_t code) :_sym(sym), _len(len), _code(code) {}
+        elem(const uint8_t& sym) : _sym(sym) {}
+        bool operator<(const elem& rhs) const {
+            if (_len < rhs._len)
+                return true;
+            else if (_len > rhs._len)
+                return false;
+            else
+                return _sym < rhs._sym;
+        }
+    };
+    std::vector<elem> _table;
+public:
+    hufstr();
+    std::vector<uint8_t> compress(const std::string& s) const;
+    std::string decompress(const std::vector<uint8_t>& v) const;
+};
